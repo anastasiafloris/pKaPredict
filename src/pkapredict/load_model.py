1eb279ae181fa02abe79375450375e3aa7040349
anastasiafloris <anastasia.floris@epfl.ch>
2025-04-16 19:58:58 +0200

    changed load_model module

--- src/pkapredict/load_model.py
+++ src/pkapredict/load_model.py
@@ -3,12 +3,12 @@ import pickle
 
 def load_model(): 
     """
-    Load a pickled LGBMRegressor model from the package's models directory.
-    """
-    base_dir = os.path.dirname(os.path.abspath(__file__))  # Get current script directory
-    model_path = os.path.abspath(os.path.join(base_dir, "..", "..", "notebooks", "models", "best_pKa_model.pkl"))
+    Load a pickled LGBMRegressor trained model from the package's models directory.
 
-    
+    """
+    base_dir = os.path.dirname(os.path.abspath(__file__))  
+    model_path = os.path.join(base_dir, "models", "best_pKa_model.pkl")
+
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"❌ Model file not found at: {model_path}")
 
@@ -16,6 +16,7 @@ def load_model():
         model = pickle.load(file)
     print("✅ LGBMRegressor model successfully loaded!")
     return model
+
 
 
 
