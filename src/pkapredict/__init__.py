7e29dbd3dc064b8bdd50764263e51e8e9b956103
anastasiafloris <anastasia.floris@epfl.ch>
2025-05-05 21:41:35 +0200

    data preprocessing

--- src/pkapredict/__init__.py
+++ src/pkapredict/__init__.py
@@ -1,10 +1,12 @@
 """pKaPredict project."""
 
-from .clean_and_visualize_pka import clean_and_visualize_pka
+
 from .load_model import load_model
 from .plot_data import plot_data
 from .predict_pKa import predict_pKa
 from .RDkit_descriptors import RDkit_descriptors
 from .smiles_to_rdkit_descriptors import smiles_to_rdkit_descriptors
+from .data_preprocessing import download_raw_dataset, preview_data, clean_and_visualize_pka
+
 
 __version__ = "0.1.7"
